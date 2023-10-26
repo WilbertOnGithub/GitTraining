1c4b6d3407c19c3d0853f55d0c51eb41160ed10a
Wilbert van Dolleweerd <wilbert@arentheym.com>
2023-10-26 09:50:23 +0200

    Update install script to use Winget

--- scripts/configure.cmd
+++ scripts/configure.cmd
@@ -1,8 +1,33 @@
 @ECHO OFF
 
+IF EXIST "%LOCALAPPDATA%\Microsoft\WindowsApps\Winget.Exe" (
+	ECHO "Winget found - using it to install software"
+	GOTO WinGetInstalled
+)
+ECHO Could not find Winget. 
+ECHO Please check if you have installed it.
+ECHO Re-run the script after fixing this problem.
+EXIT /B 1
+
+:WinGetInstalled
+
+REM Use Winget to install all dependencies
+
+winget install -e --id Git.Git
+winget install -e --id Notepad++.Notepad++
+winget install -e --id Perforce.P4Merge
+
+REM Add Git to system path (and also to the current session)
+SETX /M PATH "%PATH%;C:\Program Files\Git"
+PATH "%PATH%;C:\Program Files\Git"
+
+REM Configure Git to use P4Merge
+
 git config --global merge.tool p4merge
 git config --global diff.tool p4merge
 git config --global mergetool.keepBackup false
+
+REM Set Notepad++ as main editor
 
 :ConfigureNotepad
 IF EXIST "C:\Program Files\Notepad++\notepad++.exe" (
@@ -17,6 +42,8 @@ ECHO Could not find Notepad++ at Program Files\Notepad++ or Program Files (x86)\
 ECHO Please check if you have installed it.
 ECHO Re-run the script after fixing this problem.
 EXIT /B 1
+
+REM Explicitly set path to P4Merge
 
 :ConfigureP4Merge
 IF EXIST "C:\Program Files\Perforce\p4merge.exe" (
